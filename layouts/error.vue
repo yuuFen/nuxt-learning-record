c3d2cf00e06cf461fa74e6e959d51776d266bdc0
Yu <me@yuufen.com>
2020-03-27 15:03:23 +0800

    配置布局 / 错误页

---
+++ layouts/error.vue
@@ -0,0 +1,19 @@
+<template>
+  <!-- 错误页 -->
+  <div>
+    <h1 v-if="error.statusCode === 404">页面不存在</h1>
+    <h1 v-else>应用发生错误异常</h1>
+    <p>{{ error }}</p>
+    <nuxt-link to="/">首 页</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  // 服务器会传递一个 error 对象
+  props: ["error"],
+  layout: "blank"
+};
+</script>
+
+<style></style>
---
+++ pages/login.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h2>login</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  // 设置 layout
+  layout: "blank"
+};
+</script>
+
+<style></style>
